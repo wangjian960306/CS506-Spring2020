57b07dba5de16a349315167ac2dc33e6759eef2b
wangj <wangjian960306@gmail.com>
2020-01-27 17:30:27 -0500

    fill lake.py

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,8 @@
 def draw_lake():
-    print("lake not found")
+    if (width < 0 or length < 0):
+        raise ValueError("invalid width or length")
+    for i in range(width):
+        for j in range(length):
+            print("~",end="")
+        print() 
     return
